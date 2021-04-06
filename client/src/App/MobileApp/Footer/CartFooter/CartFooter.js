623b8dc18d48919cc84be1acdef455de3c550e46
Itay <zuker2085@gmail.com>
2021-04-06 16:17:10 +0300

    cart footer fix

--- client/src/App/MobileApp/Footer/CartFooter/CartFooter.js
+++ client/src/App/MobileApp/Footer/CartFooter/CartFooter.js
@@ -6,7 +6,7 @@ import './cart_footer.scss'
 
 const CartFooter = () => {
 
-    const { checkout, setAppState } = useContext( ShopContext )
+    const { checkout, setAppState, appState } = useContext( ShopContext )
 
     const history = useHistory()
 
@@ -26,8 +26,8 @@ const CartFooter = () => {
         <div className='cart_footer_container'>
             <LanguageButton />
             <h4
-                onClick={ () => { goTo( 'shop' )} }
-                >Shop <i className="fas fa-store"></i>
+                onClick={ () => { goTo( 'shop' )} }>
+                    { appState.language === 'english' ? 'Shop' : 'חנות' } <i className="fas fa-store"></i>
             </h4>
         </div>
     )
